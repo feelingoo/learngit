aa08a67e5d2cb1222f9beb413e99345f4e81675c
feelingoo <uioqop@163.com>
2018-04-18 17:09:45 +0800

    3

--- Application.java
+++ Application.java
@@ -5,8 +5,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @123
 @EnableScheduling
+
+
+
+
+
+
 public class Application {
 
     public static void main(String[] args) {
